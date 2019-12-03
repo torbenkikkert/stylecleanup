1ce83b344d0d39d901dbfd97fce9bba2c5e94490
Torben Kikkert <tk@connectedcars.io>
2019-12-03 16:18:51 +0100

    add nullishCoalescingOperator

--- analyzeFile.js
+++ analyzeFile.js
@@ -33,7 +33,7 @@ module.exports = (file) => {
   const text = fs.readFileSync(file, 'utf8')
   const ast = babylon.parse(text, {
     sourceType: 'module',
-    plugins: ['jsx', 'typescript', 'objectRestSpread', 'classProperties', 'dynamicImport', 'optionalChaining'],
+    plugins: ['jsx', 'typescript', 'objectRestSpread', 'classProperties', 'dynamicImport', 'optionalChaining', 'nullishCoalescingOperator'],
   })
   const lines = text.split('\n')
 
